b4e95d1d910a929016661a8bef58a647b84b26e8
tedlittledale <ted.littledale@gmail.com>
2024-08-16 10:07:40 +0100

    complete tutorial

---
+++ app/api/chat/route.ts
@@ -0,0 +1,45 @@
+import { createResource } from "@/lib/actions/resources";
+import { openai } from "@ai-sdk/openai";
+import { convertToCoreMessages, streamText, tool } from "ai";
+import { findRelevantContent } from "@/lib/ai/embedding";
+import { z } from "zod";
+
+// Allow streaming responses up to 30 seconds
+export const maxDuration = 30;
+
+export async function POST(req: Request) {
+  const { messages } = await req.json();
+
+  const result = await streamText({
+    model: openai("gpt-4o"),
+    system: `You are a helpful assistant. Check your knowledge base before answering any questions.
+    Only respond to questions using information from tool calls.
+    if no relevant information is found in the tool calls, respond, "Sorry, I don't know."`,
+    messages: convertToCoreMessages(messages),
+    tools: {
+      addResource: tool({
+        description: `add a resource to your knowledge base.
+          If the user provides a random piece of knowledge unprompted, use this tool without asking for confirmation.`,
+        parameters: z.object({
+          content: z
+            .string()
+            .describe("the content or resource to add to the knowledge base"),
+        }),
+        execute: async ({ content }) => createResource({ content }),
+      }),
+      getInformation: tool({
+        description: `get information from your knowledge base to answer questions.`,
+        parameters: z.object({
+          question: z.string().describe("the users question"),
+        }),
+        execute: async ({ question }) => {
+          const relevantContent = await findRelevantContent(question);
+          console.log({ relevantContent });
+          return relevantContent;
+        },
+      }),
+    },
+  });
+
+  return result.toDataStreamResponse();
+}
